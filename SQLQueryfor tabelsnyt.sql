abff539ad6aef390b40a7ebdc1b3139cdfbb8c5a
R4wdha19 <116624370+R4wdha19@users.noreply.github.com>
2023-01-26 16:59:18 +0400

    SQL

--- SQLQueryfor tabelsnyt.sql
+++ SQLQueryfor tabelsnyt.sql
@@ -7,35 +7,60 @@ publisher varchar(100),
 name varchar(100), 
 title varchar(100), 
 author varchar(100),
+list_name varchar(100) ,
+published_date_description varchar(100),
+published_date varchar(100),
+rank int,
+updated varchar(100));
+drop table authors;
 
-	list_name varchar(100) ,
-	published_date_description varchar(100),
-	published_date varchar(100) ,
-	updated varchar(100));
-
-	select * from authors;
-	select * from Sections;
 	create table Sections (
 	id integer PRIMARY KEY IDENTITY(1,1),
 	hits int,
-	source VARCHAR (100),
-	section_name VARCHAR (100),
-	subsection_name VARCHAR (100),
-	 pub_date VARCHAR (100),
-	document_type VARCHAR (100),
-	lead_paragraph  VARCHAR (100),
-	status VARCHAR (100)
+	source VARCHAR (5000),
+	section_name VARCHAR (5000),
+	subsection_name VARCHAR (5000),
+	 pub_date VARCHAR (5000),
+	document_type VARCHAR (5000),
+	lead_paragraph  VARCHAR (5000),
+	status VARCHAR (5000)
 	);
 	Alter table Sections ALTER COLUMN  lead_paragraph VARCHAR (5000);
-
-		create table Articls (
+	drop table Sections;
+		create table Articles (
 	id integer PRIMARY KEY IDENTITY(1,1),
 	hits int,
-	source VARCHAR (100),
-	section_name VARCHAR (100),
-	subsection_name VARCHAR (100),
-	 pub_date VARCHAR (100),
-	document_type VARCHAR (100),
-	lead_paragraph  VARCHAR (100),
-	status VARCHAR (100)
+	source VARCHAR (5000),
+	section_name VARCHAR (5000),
+	subsection_name VARCHAR (5000),
+	 pub_date VARCHAR (5000),
+	document_type VARCHAR (5000),
+	lead_paragraph  VARCHAR (5000),
+	status VARCHAR (5000),
+	name  VARCHAR (5000),
+	value VARCHAR (5000),
+	rank int
 	);
+
+		select * from authors;
+	select * from Sections;
+	
+	select * from Articles;
+	delete from Articles where id is not null;
+	drop table Articles;
+
+	-- What are the top 5 sections with the most articles.
+	select Distinct subsection_name, document_type,COUNT(*) AS 'NumberOFArticles'
+	from Sections group by document_type,subsection_name;
+
+--How many articles were written by each author?
+Alter table articles add author_id int;
+Alter table articles add Section_id int;
+
+
+
+
+	 
+	
+	
+	
